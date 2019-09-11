623c9b93d5659fc0b5d355de8dd52ee8e1748743
Justin Kim <38806488+naznsan@users.noreply.github.com>
2019-09-11 14:55:02 +1200

    Added typed.js library and new type-like interface on terminal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-setInterval(function() {
-    $("#blink").fadeToggle(250);
-}, 500);
+// Typing effect for intro words on terminal
+// typed.js library used from https://github.com/mattboldt/typed.js
+var options = {
+    strings: ["Programmer", "Thinker", "Learner"],
+    typeSpeed: 50,
+    loop: true,
+    cursorChar: '_'
+}
+
+var typed = new Typed("#typed", options);
